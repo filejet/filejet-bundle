3603130a7dde5dd5c2244d4a219110e13a027b6a
Ivan Barlog <ivanbarlog@users.noreply.github.com>
2021-12-06 16:51:38 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,20 +11,27 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('file_jet');
+        $treeBuilder = new TreeBuilder('file_jet');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('file_jet');
+        }
+
         $rootNode
             ->children()
-                ->scalarNode('storage_id')->isRequired()->end()
-                ->scalarNode('api_key')->isRequired()->end()
-                ->scalarNode('signature_secret')->defaultNull()->end()
-                ->scalarNode('base_url')->defaultNull()->end()
-                ->booleanNode('auto_mode')->defaultTrue()->end()
-                ->scalarNode('custom_domain')
-                    ->defaultNull()
-                    ->info('Custom domain to use, without scheme')
-                    ->example('cdn.example.com')
-                ->end()
+            ->scalarNode('storage_id')->isRequired()->end()
+            ->scalarNode('api_key')->isRequired()->end()
+            ->scalarNode('signature_secret')->defaultNull()->end()
+            ->scalarNode('base_url')->defaultNull()->end()
+            ->booleanNode('auto_mode')->defaultTrue()->end()
+            ->scalarNode('custom_domain')
+            ->defaultNull()
+            ->info('Custom domain to use, without scheme')
+            ->example('cdn.example.com')
+            ->end()
             ->end();
 
         return $treeBuilder;
